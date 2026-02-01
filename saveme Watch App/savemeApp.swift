3a2ed0dfc18cb2660905b5ac82d90f50a592bf64
adirdavi <adir.davidov@s.afeka.ac.il>
2026-02-01 15:55:17 +0200

    Initial Commit

---
+++ saveme Watch App/savemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  savemeApp.swift
+//  saveme Watch App
+//
+//  Created by Adir Davidov on 01/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct saveme_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ saveme/savemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  savemeApp.swift
+//  saveme
+//
+//  Created by Adir Davidov on 01/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct savemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
