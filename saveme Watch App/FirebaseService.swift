30b5d4907d8a8a5280789c193ca78b6b4a805771
adirdavi <adir.davidov@s.afeka.ac.il>
2026-04-22 22:24:34 +0300

    KAN-25: Implemented Health Matrix (TriageEngine), fixed GPS location updates, and cleaned Watch UI

---
+++ saveme Watch App/FirebaseService.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseService.swift
+//  saveme Watch App
+//
+//  Created by Adir Davidov on 22/04/2026.
+//
+
+import Foundation
---
+++ saveme Watch App/LocationService.swift
@@ -0,0 +1,8 @@
+//
+//  LocationService.swift
+//  saveme Watch App
+//
+//  Created by Adir Davidov on 22/04/2026.
+//
+
+import Foundation
